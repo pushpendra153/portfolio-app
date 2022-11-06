a66d9f98f5f3c0f025182cc6c884dbfe7600358a
pushpendra153 <78480462+pushpendraTTN@users.noreply.github.com>
2022-11-07 03:03:51 +0530

    added component folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from './components/Shared/button'
 
 function App() {
   return (
     <div className="App">
     <h1>Portfolio App</h1>
+    <Button />
     </div>
   );
 }
